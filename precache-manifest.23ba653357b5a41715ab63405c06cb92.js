4c52a50c0e0ba4633d043430888f7d5efaae13e0
mathxi <35033114+mathxi@users.noreply.github.com>
2019-10-14 13:25:18 +0200

    init vrais

---
+++ precache-manifest.23ba653357b5a41715ab63405c06cb92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "be0b56f20d4d2e68374a",
+    "url": "/js/chunk-vendors.f0419988.js"
+  },
+  {
+    "revision": "7e70d57c0f34f6277e6a",
+    "url": "/js/app.313bef6e.js"
+  },
+  {
+    "revision": "c8724834f3a5c1673079",
+    "url": "/js/about.145c7545.js"
+  },
+  {
+    "revision": "6f9b245438dd995a5a1da03e6cece51b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7e70d57c0f34f6277e6a",
+    "url": "/css/app.ba629074.css"
+  }
+];
